2c82bcd1e47d54e6034e0fd5b87370b008523e0a
Gerard Darris <darrisgr@gmail.com>
2023-02-16 10:31:46 -0600

    Updated index.html, scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function makeOrange() {
+    let body = document.querySelector("body");
+    body.style.backgroundColor = "orange";
+}
+function absolveMeOfSin() {
+    let body = document.querySelector("body");
+    body.style.backgroundColor = "";
+}

ab93f68726003a694c6070508af3f4f9526545ef
Gerard Darris <darrisgr@gmail.com>
2023-02-16 10:44:27 -0600

    Updated index.html, scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,35 @@
-function makeOrange() {
+window.addEventListener("load", function() {
     let body = document.querySelector("body");
-    body.style.backgroundColor = "orange";
-}
-function absolveMeOfSin() {
-    let body = document.querySelector("body");
-    body.style.backgroundColor = "";
-}
+    let orange = document.getElementById("orange");
+    let regrets = document.getElementById("regrets");
+    let troll = document.getElementById("troll");
+    let fixer = document.getElementById("cleanup");
+
+    orange.addEventListener("click", function() {
+        body.style.backgroundColor = "orange";
+    });
+
+    regrets.addEventListener("click", function() {
+        body.style.backgroundColor = "";
+    })
+
+    troll.addEventListener("mouseover", function() {
+        troll.innerHTML = "YOU DUN GOOFED NOW 😈"
+        troll.style.fontSize = "50px";
+        body.style.backgroundColor = "#ff1100";
+        regrets.disabled = true;
+        orange.disabled = true;
+    });
+
+    troll.addEventListener("mouseout", function() {
+        troll.innerHTML = "Hello there. 🙂"
+        body.style.backgroundColor = "";
+        fixer.style.display = "";
+    });
+
+    fixer.addEventListener("click", function() {
+        regrets.disabled = false;
+        orange.disabled = false;
+        troll.style.fontSize = "13px";
+    });
+});
